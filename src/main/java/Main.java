fe210251a9261f8acfc553ed72670006b791e612
Aldric-Ducreux <neogeekmo@live.fr>
2019-11-06 15:34:11 +0100

    #7 add class User, Customer, Member, Employee
    Ajout des classes de la classes abstraite User et des différents type d'utilisateur dépendant d'user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+
+import user.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        new Customer(1, null, null, -99, null, null, 0);
     }
 }
---
+++ src/main/java/user/Customer.java
@@ -0,0 +1,8 @@
+package user;
+
+public class Customer extends User {
+
+    public Customer(int id, String firstname, String name, int age, String mail, String adress, int countCookie) {
+        super(id, firstname, name, age, mail, adress, countCookie);
+    }
+}
