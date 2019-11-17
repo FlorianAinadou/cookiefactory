5cea19c4091bb4dddc6f5a7fa26abe03f7c5f8dc
Virgile <virgile.fantauzzi@hotmail.fr>
2019-11-17 21:47:13 +0100

    Add Order equal and hashCode #10

--- src/main/java/order/Order.java
+++ src/main/java/order/Order.java
@@ -1,12 +1,15 @@
 package order;
 
+import com.sun.istack.internal.Nullable;
 import customer.Customer;
 
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * @author Florian AINADOU
  * @author Lydia BARAUKOVA
+ * @author Virgile FANTAUZZI
  */
 public class Order {
     private final int id;  // just a  random number given to every order
@@ -24,9 +27,36 @@ public class Order {
         this.place = place;
     }
 
-    public int getId() { return id; }
-    public Cart getCart() { return cart; }
-    public Customer getCustomer() { return customer; }
-    public Date getDate() { return date; }
-    public Place getPlace() { return place; }
+    public int getId() {
+        return id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (!(obj instanceof Order)) return false;
+        return (((Order) obj).getId() == (this.getId()));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getDate());
+    }
 }
