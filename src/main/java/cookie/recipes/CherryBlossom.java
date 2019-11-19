334e0c1f2c3d02940f71468ecaf4be1fadbfefc7
Virgile <virgile.fantauzzi@hotmail.fr>
2019-11-19 09:49:40 +0100

    refactor constructor cookies

--- src/main/java/cookie/recipes/CherryBlossom.java
+++ src/main/java/cookie/recipes/CherryBlossom.java
@@ -3,16 +3,19 @@ package cookie.recipes;
 import cookie.Cookie;
 import cookie.parameters.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author Lydia BARAUKOVA
+ * @author Virgile FANTAUZZI
  */
 public class CherryBlossom extends Cookie {
 
 
     public CherryBlossom() {
-        super(CookieName.cherryBlossom.toString(), Dough.cherryJam, Cooking.chewy, Flavour.cherry, Mix.topped, new ArrayList<>(), 0.23);
-        toppings.add(Topping.cherrySyrup);
+        super(CookieName.cherryBlossom.toString(), Dough.cherryJam, Cooking.chewy, Flavour.cherry, Mix.topped, new ArrayList<>(Collections.singletonList(Topping.cherrySyrup)), 0.23);
     }
 }
--- src/main/java/cookie/recipes/Chocolala.java
+++ src/main/java/cookie/recipes/Chocolala.java
@@ -4,16 +4,17 @@ import cookie.Cookie;
 import cookie.parameters.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * @author Lydia BARAUKOVA
+ * @author Virgile FANTAUZZI
  */
 public class Chocolala extends Cookie {
 
 
     public Chocolala() {
-        super(CookieName.chocolala.toString(), Dough.chocolate, Cooking.chewy, Flavour.chocolate, Mix.topped, new ArrayList<Topping>(), 0.25);
-        toppings.add(Topping.milkChocolate);
+        super(CookieName.chocolala.toString(), Dough.chocolate, Cooking.chewy, Flavour.chocolate, Mix.topped, new ArrayList<>(Collections.singletonList(Topping.milkChocolate)), 0.25);
     }
 
 
