a43fe74cb72ac6b78249bf4e8bac8db482ea0893
Lydia Baraukova <livelinndy@gmail.com>
2019-11-19 10:02:07 +0100

    Cookie price depends on the price of its ingredients

--- src/main/java/cookie/Cookie.java
+++ src/main/java/cookie/Cookie.java
@@ -12,23 +12,24 @@ import cookie.parameters.*;
  */
 public class Cookie {
 
-    private final String name;
-    private Dough dough;
-    private Cooking cooking;
-    private Flavour flavour;
-    private Mix mix;
+    protected final String name;
+    protected Dough dough;
+    protected Cooking cooking;
+    protected Flavour flavour;
+    protected Mix mix;
     protected List<Topping> toppings;
-    private double unitPriceEuro;
-    private String type;
+    protected double unitPriceEuro;
+    protected String type;
 
-    public Cookie(String n, Dough d, Cooking c, Flavour f, Mix m, List<Topping> t, double p) {
+    public Cookie(String n, Dough d, Cooking c, Flavour f, Mix m, List<Topping> t) {
         name = n;
         dough = d;
         cooking = c;
         flavour = f;
         mix = m;
         toppings = t;
-        unitPriceEuro = p;
+        unitPriceEuro = dough.getPrice() + flavour.getPrice();
+        toppings.forEach(topping -> unitPriceEuro += topping.getPrice());
     }
 
     public String getName() { return name; }
